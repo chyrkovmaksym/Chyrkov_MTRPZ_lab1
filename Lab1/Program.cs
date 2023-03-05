217e2de7555983d307774c232acbe22c16774f81
Maksym Chyrkov <89487067+chyrkovmaksym@users.noreply.github.com>
2023-03-05 21:58:34 +0200

    feat: added function for getting values from console (interactive mode)

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -33,9 +33,34 @@ List<double> GetValuesFromFile(string path)
     return values;
 }
 
+List<double> GetValuesFromConsole()
+{
+    List<double> values = new List<double>();
+    char[] coeff = { 'a', 'b', 'c' };
+    foreach (char symb in coeff)
+    {
+        double value;
+        Console.Write($"{symb} = ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        string input = Console.ReadLine();
+        while (!Double.TryParse(input, out value))
+        {
+            Console.ResetColor();
+            Console.WriteLine($"Error. Expected a valid real number, got {input} instead");
+            Console.Write($"{symb} = ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            input = Console.ReadLine();
+        }
+        values.Add(value);
+        Console.ResetColor();
+    }
+    return values;
+}
+
+
 void main()
 {
-    List<double> coeff = GetValuesFromFile(filePath);
+    List<double> coeff = GetValuesFromConsole();
     for(int i = 0; i < coeff.Count; i++)
     {
         Console.WriteLine(coeff[i]);
