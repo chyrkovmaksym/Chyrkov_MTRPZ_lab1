1909cacd4b04fa6094f676cabdbc07dfea8b4534
Maksym Chyrkov <89487067+chyrkovmaksym@users.noreply.github.com>
2023-03-05 21:56:47 +0200

    feat: created function for reading coefficients from file (file mode)

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using System;
+using System.IO;
+
+string filePath = "file.txt";
+
+List<double> GetValuesFromFile(string path)
+{
+    List<double> values = new List<double>();
+    try
+    {
+        using (StreamReader reader = new StreamReader(path))
+        {
+            var fileContent = reader.ReadLine();
+            if (fileContent != null)
+            {
+                foreach (string s in fileContent.Split(" "))
+                {
+                    double value = 0;
+                    if (!Double.TryParse(s, out value))
+                    {
+                        Console.WriteLine("Invalid input");
+                        return null;
+                    }
+                    values.Add(value);
+                }
+            }
+        }
+    }
+    catch (IOException e)
+    {
+        Console.WriteLine($"An error occurred while reading the file: {e.Message}");
+    }
+    return values;
+}
+
+void main()
+{
+    List<double> coeff = GetValuesFromFile(filePath);
+    for(int i = 0; i < coeff.Count; i++)
+    {
+        Console.WriteLine(coeff[i]);
+    }
+}
+
+main();
